4c0f8dbc28da8e809a949d29c35a207ec38b888d
souvik666 <peacedynasty0@gmail.com>
2022-08-20 12:45:58 +0530

    api with rest_framework

---
+++ myapp/api/views.py
@@ -0,0 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from  base.models import Item
+from .serializer import ItemSerializer
+
+
+@api_view(['GET'])
+def getData(request):
+    items = Item.objects.all()
+    serializedValue = ItemSerializer(items, many=True)
+    return Response(serializedValue.data)
+
+
+
+@api_view(['POST'])
+def addItem(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
